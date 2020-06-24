0dbc16fe16a35624b5eb6c4f8cabd95fbbac508b
Iris Wu <iriswu@IrisdeMacBook-Air.local>
2020-06-24 10:13:12 -0400

    update Jun 24, 2020

--- main.py
+++ main.py
@@ -7,15 +7,15 @@ Input: [-2,1,-3,4,-1,2,1,-5,4],
 Output: 6
 Explanation: [4,-1,2,1] has the largest sum = 6.
 """
-class Solution53:
-    def maxSubArray(self, nums: List[int]) -> int:
-        n = len(nums)
-        max_sum = nums[0]
-        for i in range(1,n):
-            if nums[i-1] > 0:
-                nums[i] += nums[i-1]
-            max_sum = max(nums[i], max_sum)
-        return max_sum
+# class Solution53:
+#     def maxSubArray(self, nums: List[int]) -> int:
+#         n = len(nums)
+#         max_sum = nums[0]
+#         for i in range(1,n):
+#             if nums[i-1] > 0:
+#                 nums[i] += nums[i-1]
+#             max_sum = max(nums[i], max_sum)
+#         return max_sum
 
 """
 70. Climbing Stairs
@@ -50,6 +50,82 @@ class Solution70:
 
         return nums[1]
 
-n = 2
-y = Solution70
-print (y.climbStairs(n))
+# n = 2
+# y = Solution70
+# print (y.climbStairs(n))
+"""
+101. Symmetric Tree
+Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+
+For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+ 
+But the following [1,2,2,null,3,null,3] is not:
+
+    1
+   / \
+  2   2
+   \   \
+   3    3
+"""
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+# 1. recursive solution
+class Solution:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        def helper(one, two):
+            if (not one) and (not two):
+                return True
+            if (not one) or (not two):
+                return False
+            if one.val != two .val:
+                return False
+            return helper(one.left, two.right) and helper(one.right, two.left)
+        if (not root):
+            return True
+        return helper (root.left, root.right)
+
+"""
+104. Maximum Depth of Binary Tree
+Given a binary tree, find its maximum depth.
+The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+Note: A leaf is a node with no children.
+
+Example:
+Given binary tree [3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its depth = 3.
+"""
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        stack = []
+        if root is not None:
+            stack.append((1,root))
+        depth = 0
+        while stack != []:
+            current_depth,root = stack.pop()
+            if root is not None:
+                depth = max(depth, current_depth)
+                stack.append((current_depth+1, root.left))
+                stack.append((current_depth+1, root.right))
+        return depth
